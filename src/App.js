92117b21e9b4216afb5324409cd9b8dd1024dc03
lmarfeo <marfeol@bc.edu>
2025-02-03 11:09:32 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -10,14 +10,11 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/lenamarfeo">
       <Header />
       <div className="p-4">
         <Routes>
-          {/* Set Home as the default route */}
-          <Route path="" element={<Home />} />
           <Route path="/" element={<Home />} />
-          <Route path="/lenamarfeo/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/gallery" element={<Gallery />} />
